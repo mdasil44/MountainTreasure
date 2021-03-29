40375780e7102bd8967e779933dea57eefba52eb
unknown <david.le454@gmail.com>
2021-03-29 15:08:36 -0400

    proper title screen

--- src/UI/TitleScreen.gd
+++ src/UI/TitleScreen.gd
@@ -2,8 +2,8 @@ extends PanelContainer
 
 func _on_StartButton_pressed():
 	get_tree().change_scene("res://src/Levels/World.tscn")
+	queue_free()
+	#visible = false
 	
-
-
 func _on_ExitButton_pressed():
 	get_tree().quit()
