363c8ad563e35d116f4c17512fef86a621bd23fd
Dani <dmyers24@uwo.ca>
2021-04-02 13:43:51 -0400

    Added enemy clear and potion spawn to Level 1

--- src/Levels/Level.gd
+++ src/Levels/Level.gd
@@ -5,12 +5,38 @@ export var required_keys = 1
 export (String) var next_scene
 
 
+var potion = preload("res://src/Potions/Potion.tscn")
+
+var room_potions = [potion, potion, potion, potion, potion, potion]
+
+var rooms = [1, 1, 1, 1, 1, 1]
+
+onready var positions = [$YSort/Room1/Position2D, $YSort/Room2/Position2D, $YSort/Room3/Position2D, $YSort/Room4/Position2D, $YSort/Room5/Position2D, $YSort/Room6/Position2D]
+
+var rng = RandomNumberGenerator.new()
+
+var roomNodes = []
+
+
 func _ready() -> void:
 	$UILayer/FadeIn.fade_out()
 	yield($UILayer/FadeIn, "fade_finished")
 	$UILayer/FadeIn.hide()
 	
 	$LevelTransition.connect("body_entered", self, "next_level")
+
+	roomNodes = get_tree().get_nodes_in_group("Level1_Rooms")
+
+func _physics_process(delta):
+	if roomNodes.empty():
+		return
+	
+	for i in 6:
+		if roomNodes[i].get_child_count() == 1 && rooms[i] == 1:
+			rooms[i] = 0
+			spawn_potion(i)
+			
+	
 
 
 func next_level(body: Node) -> void:
@@ -21,4 +47,15 @@ func next_level(body: Node) -> void:
 			yield($UILayer/FadeIn, "fade_finished")
 			get_tree().change_scene(next_scene)
 			body.stats.set_keys(0)
+			
+func spawn_potion(var index):
+	rng.randomize()
+	var randomNum = rng.randi_range(0,1)
+	print(randomNum)
+	var potion_instance = room_potions[index].instance()
+	potion_instance.type = randomNum
+	positions[index].add_child(potion_instance)
+	
+	
+	
 
